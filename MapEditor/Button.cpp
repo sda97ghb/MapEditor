b53d70e855ccd0816288ba474c050fa105e05558
sda97ghb <sda97g@gmail.com>
2017-03-05 21:37:08 +0500

    Added overloading for Button class's method setSize passing two float arguments width and height.

--- MapEditor/Button.cpp
+++ MapEditor/Button.cpp
@@ -1,6 +1,16 @@
 #include "MapEditor/Button.h"
 #include "MapEditor/PointTester.h"
 
+void Button::setSize(const sf::Vector2f& size)
+{
+    sf::RectangleShape::setSize(size);
+}
+
+void Button::setSize(float width, float height)
+{
+    sf::RectangleShape::setSize(sf::Vector2f(width, height));
+}
+
 void Button::testForClick(const sf::Vector2f& pos)
 {
     if (PointTester::test(pos, *this))
